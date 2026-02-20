050776328d426f91b76f1c4fddee4694c76eea1c
YashSinghal2001 <ysinghal30@gmail.com>
2026-02-20 17:15:47 +0530

    Fixed TS errors and removed protect middleware

--- server/routes/progressRoutes.ts
+++ server/routes/progressRoutes.ts
@@ -5,15 +5,14 @@ import {
   getStats,
   getReportingStats
 } from '../controllers/progressController';
-import { protect } from '../middleware/authMiddleware';
 
 const router = express.Router();
 
 router.route('/measurements')
-  .put(protect, updateBodyMeasurements as any)
-  .get(protect, getBodyMeasurements as any);
+.put(updateBodyMeasurements as any)
+.get(getBodyMeasurements as any);
 
-router.get('/stats', protect, getStats as any);
-router.get('/reporting', protect, getReportingStats as any);
+router.get('/stats', getStats as any);
+router.get('/reporting', getReportingStats as any);
 
 export default router;
